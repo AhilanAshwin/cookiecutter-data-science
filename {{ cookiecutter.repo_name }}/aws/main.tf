f8543fd27c4b18185a962a09ef114ef9c7206e96
AhilanAshwin <ahilanashwin@gmail.com>
2022-09-12 00:33:05 +0800

    Init terraform configuration for s3 bucket

--- {{ cookiecutter.repo_name }}/aws/main.tf
+++ {{ cookiecutter.repo_name }}/aws/main.tf
@@ -1,3 +1,11 @@
 #   ----------------------------------------------------------------
 #   The main configuration file for the aws infrastructure
 #   ----------------------------------------------------------------
+
+# Create an s3 bucket for data and model store
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.4.0"
+  bucket  = var.repo_name
+  tags    = local.common_tags
+}
