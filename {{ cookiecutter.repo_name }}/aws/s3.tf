5be8695facca15958e139f40166bc2585edaf39f
AhilanAshwin <ahilanashwin@gmail.com>
2022-09-13 16:42:11 +0800

    Reformat s3 bucket name

--- {{ cookiecutter.repo_name }}/aws/s3.tf
+++ {{ cookiecutter.repo_name }}/aws/s3.tf
@@ -6,7 +6,7 @@
 module "s3-bucket" {
   source        = "terraform-aws-modules/s3-bucket/aws"
   version       = "3.4.0"
-  bucket_prefix = "{{ cookiecutter.s3_bucket if "[OPTIONAL]" not in cookiecutter.s3_bucket else cookiecutter.repo_name }}"
+  bucket_prefix = "{{ cookiecutter.s3_bucket if '[OPTIONAL]' not in cookiecutter.s3_bucket else cookiecutter.repo_name }}"
   tags          = local.common_tags
 }
 
